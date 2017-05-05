86caddccbc88f9371428e281c69cb85eee68defd
GeoffWacker <geoffwacker@gmail.com>
2017-05-04 20:02:28 -0700

    Added non-pwm motor driver

--- web-server/motor_driver_pwm.py
+++ web-server/motor_driver_pwm.py
@@ -13,32 +13,33 @@ class MotorDriver:
         self.duty = 0
         self.EN_pin = EN_pin
         GPIO.output(self.EN_pin, GPIO.LOW)
-        
-        self.FW_pin = FW_pin
-        self.BW_pin = BW_pin
-        
-    	self.set_duty_cycle(0)
+
+        frequency = 2500 #Hz
+        self.FW_pwm = GPIO.PWM(FW_pin, frequency)
+        self.FW_pwm.start(0)
+	self.BW_pwm = GPIO.PWM(BW_pin, frequency)
+        self.BW_pwm.start(0)
+	self.set_duty_cycle(0)
 	
     def set_duty_cycle(self, duty):
         if (duty > 0):
-            GPIO.output(BW_pin, GPIO.LOW)
-            GPIO.output(FW_pin, GPIO.HIGH)
-        elif (duty < 0):
-            GPIO.output(FW_pin, GPIO.LOW)
-            GPIO.output(BW_pin, GPIO.HIGH)
+            if (duty > 100):
+                duty = 100
+            self.BW_pwm.ChangeDutyCycle(0)
+            self.FW_pwm.ChangeDutyCycle(duty)
         else:
-            GPIO.output(BW_pin, GPIO.LOW)
-            GPIO.output(FW_pin, GPIO.LOW)
-            
+            if (duty < -100):
+                duty = -100
+            self.FW_pwm.ChangeDutyCycle(0)
+            self.BW_pwm.ChangeDutyCycle(duty * -1)
         self.duty = duty
-        print("MotorDriver | 'Set' Duty Cycle: {}".format(duty))
+        print("MotorDriver | Set Duty Cycle: {}".format(duty))
 
     def get_duty(self):
         return self.duty
 
     def enable_motor(self):
         GPIO.output(self.EN_pin, GPIO.HIGH)
-        
     def disable_motor(self):
         GPIO.output(self.EN_pin, GPIO.LOW)
 
