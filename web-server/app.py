1542a5a28572792fbd09eb3ca5f697c976b305f4
GeoffWacker <geoffwacker@gmail.com>
2017-05-24 13:26:15 -0700

    Log file is now completely working, and prevents skylight from opening further than 10 seconds of travel

--- web-server/app.py
+++ web-server/app.py
@@ -1,5 +1,6 @@
 import os, time, socket
 import motor_driver
+import logger
 from datetime import timedelta
 from subprocess import check_output
 from flask import Flask, render_template, request
@@ -7,34 +8,56 @@ app = Flask(__name__)
 
 #globals
 motorDriver = None
+Logger = None
 
 @app.route("/")
 def main():
    global motorDriver
-   
+   global Logger 
    motorDriver = motor_driver.MotorDriver(25, 24, 23)
-
+   Logger = logger.Logger("log.txt")
    return render_template('main.html')
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
 @app.route("/<action>")
 def action(action):
    global motorDriver
-
+   global Logger
+   #error = None
+   #print request.args.get('error')
    if action == "on":
-      motorDriver.enable_motor()
-      motorDriver.set_duty_cycle(100)
-      time.sleep(5)
-      motorDriver.set_duty_cycle(0)
-
+      # Read from the log file.
+      status = Logger.readLog()
+      print status
+      # Only open the skylight if we've traveled less than 10 seconds.
+      # This will change based on the total travel time of operator.
+      if (status < 10):
+         motorDriver.enable_motor()
+      	 motorDriver.set_duty_cycle(100)
+         time.sleep(5)
+         motorDriver.set_duty_cycle(0)
+         # Add five seconds to log file.
+         Logger.writeLog(str(status + 5))
+      else:
+         print "Cannot open any further."
+         #error="open"
    if action == "off":
-      motorDriver.enable_motor()
-      motorDriver.set_duty_cycle(-100)
-      time.sleep(5)
-      motorDriver.set_duty_cycle(0)
-
+      # Read from the log file.
+      status = Logger.readLog()
+      print status
+      # Only close skylight if we have travel left to go.
+      if (status >= 5):
+         motorDriver.enable_motor()
+         motorDriver.set_duty_cycle(-100)
+         time.sleep(5)
+         motorDriver.set_duty_cycle(0)
+         # Subtract five seconds from log file.
+         Logger.writeLog(str(status - 5))
+      else:
+         print "Cannot close any further."
+         #error="close"
+   #return render_template('main.html', error=error)
    return render_template('main.html')
-   
 @app.route("/options")
 def options():
    # Get the uptime.
--- web-server/logger.py
+++ web-server/logger.py
@@ -2,10 +2,11 @@
 # This file covers reading and writing from the Skylux log file. The log file contains
 # a single integer that represents the number of seconds that the operator has travelled
 # from a completely closed position.
+import os
 
 class Logger:
 	def __init__(self, logFile):
-		self.logFile = logFile
+		self.logFile = os.path.join(os.path.dirname(__file__), logFile)
 		
 	# Used to reset log file to closed position (0).
 	def resetLog(self):
@@ -15,7 +16,7 @@ class Logger:
 	# Read from the log file.
 	def readLog(self):
 		with open(self.logFile, 'r') as log:
-			status = log.read()
+			status = int(log.read())
 		return status
 
 	# Overwrite whatever is in the log file.	
