56430acfb49dc1e979c2ee858dcec7df057dcdaa
James Green <jgreen23@calpoly.edu>
2017-11-06 20:31:59 -0800

    Initial Commit
    Added empty iOS application

---
+++ Skylux/Skylux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skylux
+//
+//  Created by James Green on 11/6/17.
+//  Copyright © 2017 James Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
